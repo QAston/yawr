e15e6dc299980d1fed5ad750dab54d48c6abd107
QAston <qaston@gmail.com>
2013-07-08 20:45:58 +0200

    Add toHex function which dumps packet contents to string.

--- source/protocol/packet.d
+++ source/protocol/packet.d
@@ -3,6 +3,7 @@ module protocol.packet;
 import vibe.stream.memory;
 
 import protocol.opcode;
+import std.array;
 
 class Packet 
 {
@@ -13,5 +14,61 @@ class Packet
         this.data = new MemoryStream(data);
         // invalid opcode values will be still stored in this.opcode
         this.opcode = cast(Opcode)opcode;
+    }
+
+    public string toHex()
+    {
+        ulong oldpos = data.tell;
+        scope(exit)
+            data.seek(oldpos);
+
+        import std.ascii, std.format;
+        import std.array;
+
+        import protocol.stream_utils;
+        auto dump = appender!(dchar[]);
+        
+        dump.put("|-------------------------------------------------|---------------------------------|\n");
+        dump.put("| 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F | 0 1 2 3 4 5 6 7 8 9 A B C D E F |\n");
+        dump.put("|-------------------------------------------------|---------------------------------|\n");
+
+        for (auto i = 0; i < data.size(); i += 16)
+        {
+            auto text = appender!(dchar[]);
+            auto hex = appender!(dchar[]);
+            text.put("| ");
+            hex.put("| ");
+            
+            for (auto j = 0; j < 16; j++)
+            {
+                if (j + i < data.size())
+                {
+                    data.seek(j + i);
+                    auto val = data.sread!ubyte;
+                    formattedWrite(hex , "%2X ", val);
+
+                    if (val.isPrintable)
+                        text.put(val);
+                    else
+                        text.put(".");
+                    
+                    text.put(" ");
+                }
+                else
+                {
+                    hex.put("   ");
+                    text.put("  ");
+                }
+            }
+            
+
+            hex.put(text.data ~ "|");
+            hex.put("\n");
+            dump.put(hex.data);
+        }
+
+        
+        dump.put("|-------------------------------------------------|---------------------------------|");
+        return std.conv.to!string(dump.data());
     }
 }
