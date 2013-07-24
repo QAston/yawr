d8876b05e8e2119447090f9876f5cf3d847052ce
QAston <qaston@gmail.com>
2013-07-24 08:46:05 +0200

    Wowdefs: Add ObjectType enum.

---
+++ wowdefs/wowdefs/wow_object.d
@@ -0,0 +1,15 @@
+module wowdefs.wow_object;
+
+enum ObjectType
+{
+    Object = 0,
+    Item = 1,
+    Container = 2,
+    Unit = 3,
+    Player = 4,
+    GameObject = 5,
+    DynamicObject = 6,
+    Corpse = 7,
+    AreaTrigger = 8,
+    SceneObject = 9
+}
