8955ad7f6db51ca59cb038cfba892d77a744a693
QAston <qaston@gmail.com>
2013-08-04 15:57:30 +0200

    Add util.struct_printer module

---
+++ util/util/struct_printer.d
@@ -0,0 +1,98 @@
+module util.struct_printer;
+
+import std.typetuple;
+import util.traits;
+import std.traits;
+
+string fieldsToString(T)(in T t, in string alignment="")
+{
+    import std.array;
+    import std.range;
+    auto str = appender!string();
+    static if (isSomeString!T || isSomeChar!T)
+    {
+        str.put(alignment);
+        str.put(t);
+        str.put("\n");
+    }
+    else static if (is(T : U[], U) && isSomeChar!U)
+    {
+        str.put(alignment);
+        foreach(i, element; t)
+        {
+            put(element);
+        }
+        str.put("\n");
+    }
+    else static if (is(T BASE == enum))
+    {
+        str.put(alignment);
+        str.put(std.conv.to!string(t));
+        str.put(" (");
+        str.put(std.conv.to!string(cast(BASE)t));
+        str.put(")");
+        str.put("\n");
+    }
+    else static if (isBasicType!(T))
+    {
+        str.put(alignment);
+        str.put(std.conv.to!string(t));
+        str.put("\n");
+    }
+    else static if (isForwardRange!T || isArray!T)
+    {
+        static if (isForwardRange!T)
+            auto range = t.save;
+        else
+            auto range = t;
+
+        foreach(i, element; t)
+        {
+            str.put(alignment);
+            str.put("[");
+            str.put(std.conv.to!string(i));
+            str.put("]: \n");
+            str.put(element.fieldsToString(alignment ~ "\t"));
+        }
+    }
+    else 
+    {
+        import std.regex;
+        enum ctr = ctRegex!(`[^\.]*?$`);
+        foreach (i, a; t.tupleof)
+        {
+            str.put(alignment);
+            auto m2 = match(t.tupleof[i].stringof, ctr);
+            str.put(std.conv.to!string(m2.captures[0]));
+            str.put(": \n");
+            str.put(fieldsToString(a, alignment ~ "\t"));
+        }
+    }
+    return str.data();
+}
+
+unittest {
+    import std.stdio;
+    enum Enumerator {
+        A = 1,
+        B = 40,
+    }
+    struct A {
+        struct C {
+            int i = -7;
+            string j = "tyry";
+        }
+        uint a = 54;
+        bool b = true;
+        C c;
+        C[4] d;
+        alias C D;
+        Enumerator e;
+    }
+
+    struct B{
+        bool b;
+    }
+    fieldsToString(B());
+    fieldsToString(A());
+}
