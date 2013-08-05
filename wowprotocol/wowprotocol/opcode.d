124fd350cda280463fa01c429b6db3fb96855ebd
QAston <qaston@gmail.com>
2013-08-05 08:25:13 +0200

    Add wowprotocol.opcode.hasDirection function

--- wowprotocol/wowprotocol/opcode.d
+++ wowprotocol/wowprotocol/opcode.d
@@ -6,6 +6,8 @@ module wowprotocol.opcode;
 import wowdefs.wow_version;
 import std.conv;
 
+import util.protocol.direction;
+
 enum UNKNOWN_OPCODE = 0x0000;
 mixin ("public import wowprotocol.opcode_." ~ wowVersion.to!string() ~ ";");
 
@@ -20,6 +22,41 @@ string opcodeToString(Opcode opcode)
     return "<unknown opcode> " ~ (cast(ushort)opcode).to!string;
 }
 
+unittest {
+    assert(opcodeToString(cast(Opcode)UNKNOWN_OPCODE) == "<unknown opcode> 0");
+    assert(opcodeToString(Opcode.SMSG_NEW_WORLD) == "SMSG_NEW_WORLD");
+}
+
+/+
+ + Returns true if given opcode can be sent in a given direction of client-server communication
+ +/
+bool hasDirection(Opcode opc, Direction dir)
+{
+    auto str = opcodeToString(opc);
+    switch(str[0])
+    {
+        case 'S':
+            return dir == Direction.s2c;
+        case 'C':
+            return dir == Direction.c2s;
+        case 'M':
+            return true;
+        default:
+            return false;
+    }
+}
+
+unittest {
+    assert(hasDirection(cast(Opcode)UNKNOWN_OPCODE, Direction.c2s) == false);
+    assert(hasDirection(cast(Opcode)UNKNOWN_OPCODE, Direction.s2c) == false);
+    assert(hasDirection(Opcode.SMSG_NEW_WORLD, Direction.c2s) == false);
+    assert(hasDirection(Opcode.SMSG_NEW_WORLD, Direction.s2c) == true);
+    assert(hasDirection(Opcode.CMSG_CAST_SPELL, Direction.s2c) == false);
+    assert(hasDirection(Opcode.CMSG_CAST_SPELL, Direction.c2s) == true);
+    assert(hasDirection(Opcode.MSG_MOVE_STOP, Direction.s2c) == true);
+    assert(hasDirection(Opcode.MSG_MOVE_STOP, Direction.c2s) == true);
+}
+
 private static string[Opcode] opcodeNames;
 
 static this()
@@ -32,8 +69,4 @@ static this()
         assert(value !in opcodeNames, "Two opcodes have same value: "~ member ~" and " ~ opcodeNames[value]);
         opcodeNames[value] = member ~ " (" ~ (cast(ushort)value).to!string~")";
     }
-}
-
-unittest {
-    assert(opcodeToString(cast(Opcode)UNKNOWN_OPCODE) == "<unknown opcode> 0");
 }
