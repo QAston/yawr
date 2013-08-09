20cab12a4f2d870558661ce2c7c2de6f2195a573
QAston <qaston@gmail.com>
2013-08-09 19:39:16 +0200

    Authserver is now capable of receiving and sending messages.

--- authserver/authserver/app.d
+++ authserver/authserver/app.d
@@ -1,5 +1,22 @@
 module authserver.app;
 
-void main()
+import std.stdio;
+
+import vibe.d : listenTCP, runEventLoop;
+
+import util.stream;
+
+import authserver.session;
+
+immutable ushort bindPort = 3724;
+
+shared static this()
 {
+    // Setup listening on a given port
+	listenTCP(bindPort, (conn){ (new Session(conn)).run(); });
+}
+
+int main()
+{
+    return runEventLoop();
 }
