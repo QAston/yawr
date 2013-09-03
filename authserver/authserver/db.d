d7cd563b99717146833faa1d3a2e0d2300c991f1
QAston <qaston@gmail.com>
2013-09-03 18:15:48 +0200

    Add authserver.db.formatSqlColumnsList function.

--- authserver/authserver/db.d
+++ authserver/authserver/db.d
@@ -33,3 +33,38 @@ struct AuthInfo
     string v;
     string s;
 }
+
+/++
++ Returns a comma separated list of all fields from a struct of given type T
++/
+string formatSqlColumnList(T)()
+{
+    static assert(__traits(allMembers, T).length == T.tupleof.length);
+    import std.array;
+    auto app = appender!(string);
+    bool first = true;
+    foreach (a; __traits(allMembers, T))
+    {
+        if (first)
+        {
+            first = false;
+        }
+        else
+        {
+            app.put(", ");
+        }
+        app.put(a);
+    }
+    return app.data();
+}
+
+
+unittest {
+
+    struct Test {
+        uint a;
+        ushort b;
+    }
+    
+    assert(formatSqlColumnList!(Test) == "a, b");
+}
